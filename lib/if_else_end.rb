c682a08fe64795c1dd0fecf35166e44f1f16690e
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-19 03:12:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,7 @@
 # Write your solution here
-chance_of_rain = 0.2
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-elsif (chance_of_rain >0.25 && chance_of_rain<0.75)
-  puts "Pack an umprella!"
-elsif
-  puts "Stay home and read Hegel."
+# Write your solution here
+if Time.now.to_i.even?
+  puts "Even!"
+else
+  puts "Odd!"
 end
